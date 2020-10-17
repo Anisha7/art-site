0fc62dacf881d38bd813b8029391caec62491a21
Anisha Jain <anishajain@outlook.com>
2020-10-16 23:11:27 -0700

    Navbar added

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles.css";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <p>ANISHA'S ART FOR CHARITY</p>
+    </div>
+  );
+};
+
+export default Navbar;
