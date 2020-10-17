7c0d69bf9f419712a79953f13021913ddc6953e7
Anisha Jain <anishajain@outlook.com>
2020-10-17 09:55:46 -0700

    Updates

---
+++ precache-manifest.12161e283dd569f643b1f23b0f0ab105.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53613f1bf9c0debe22f5250d735cf3c7",
+    "url": "/art-site/index.html"
+  },
+  {
+    "revision": "00e35b5b0eb6b8744478",
+    "url": "/art-site/static/css/main.0a4cd580.chunk.css"
+  },
+  {
+    "revision": "c61f1f4758a9964ec152",
+    "url": "/art-site/static/js/2.f815ec44.chunk.js"
+  },
+  {
+    "revision": "b13e737c23346402089bf79c557ecf58",
+    "url": "/art-site/static/js/2.f815ec44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00e35b5b0eb6b8744478",
+    "url": "/art-site/static/js/main.06496c66.chunk.js"
+  },
+  {
+    "revision": "e82f12d866fd9c2209d1",
+    "url": "/art-site/static/js/runtime-main.555d0975.js"
+  },
+  {
+    "revision": "42b51e25fa3bac3575ce7e4352d6e636",
+    "url": "/art-site/static/media/Skia-Regular.42b51e25.eot"
+  },
+  {
+    "revision": "7b2932a4b94d625bcdf9a342a45e659d",
+    "url": "/art-site/static/media/Skia-Regular.7b2932a4.ttf"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/art-site/static/media/logo.5d5d9eef.svg"
+  }
+]);
