d952f5801b6eb2e48e0a983e2d1087a71b34f54d
Anisha Jain <anishajain@outlook.com>
2020-10-18 12:32:57 -0700

    Added art container description

--- src/components/Art/index.js
+++ src/components/Art/index.js
@@ -3,14 +3,22 @@ import "./styles.scss";
 import pinkstatue from "../../images/art/pinkstatue.jpg";
 import goddessportrait from "../../images/art/goddessportrait.jpg";
 import pinkdesigntee from "../../images/art/pinkdesigntee.jpg";
+import humanexperimenttiny from "../../images/art/humanexperimenttiny.jpg";
 
-const ArtContainer = ({ img, alt }) => {
+const ArtContainer = ({ img, alt, status, price, size, type }) => {
   return (
     <div className="art-section-container">
       <div className="art-container">
         <img src={img} alt={alt} />
       </div>
-      <div className="description"></div>
+      <div className="description">
+        {/* Buy prints button */}
+        <p className="title">
+          {status} - {price}
+        </p>
+        <p className="size">{size}</p>
+        <p className="details">{type}</p>
+      </div>
     </div>
   );
 };
@@ -26,6 +34,10 @@ const Art = () => {
       <ArtContainer
         img={pinkdesigntee}
         alt="Classic white tee with a blond dark skin lady in pink shirt"
+      />
+      <ArtContainer
+        img={humanexperimenttiny}
+        alt="Four small paintings aligned neatly together"
       />
     </div>
   );
